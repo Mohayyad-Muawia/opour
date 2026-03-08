44ac6d313ce8726b66aa5e427ca1d889c1e6d176
Mohayyad-Muawia <mohayyad2.0@gmail.com>
2026-03-08 21:05:08 +0200

    Change font to cairo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cairo } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+const cairo = Cairo({
+	variable: "--font-cairo",
+	subsets: ["latin", "arabic"],
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -27,7 +23,7 @@ export default function RootLayout({
 			<head>
 				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
 			</head>
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+			<body className={`${cairo.variable} antialiased`}>{children}</body>
 		</html>
 	);
 }
